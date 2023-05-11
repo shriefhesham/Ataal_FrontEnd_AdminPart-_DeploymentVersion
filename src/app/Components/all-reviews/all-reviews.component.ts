0d13bf66cff36a6d71d3030cf37fd9e508b05650
shriefhesham <shriefhesham103@gmail.com>
2023-05-11 17:18:54 +0300

    Edit

--- src/app/Components/all-reviews/all-reviews.component.ts
+++ src/app/Components/all-reviews/all-reviews.component.ts
@@ -21,4 +21,18 @@ export class AllReviewsComponent implements OnInit {
   }
 
  
+  getDate(dateString: string): string {
+    const NotificationDate = new Date(dateString);
+    const diffMs = new Date().getTime() - NotificationDate.getTime();
+    const diffMins = Math.round(diffMs / 60000); // convert to minutes
+    if (diffMins < 60) {
+      return `${diffMins} minute${diffMins === 1 ? '' : 's'} ago`;
+    } else if (diffMins < 1440) {
+      const diffHours = Math.floor(diffMins / 60);
+      return `${diffHours} hour${diffHours === 1 ? '' : 's'} ago`;
+    } else {
+      const diffDays = Math.floor(diffMins / 1440);
+      return `${diffDays} day${diffDays === 1 ? '' : 's'} ago`;
+    }
+  }
 }
